8d541fb7d6f7fe3c81c98425423d4452fdaf2bfc
Syed Ali <s.cuteali@gmail.com>
2019-05-15 23:57:51 +0500

    google analytical

--- resources/views/front-end/layout/app.blade.php
+++ resources/views/front-end/layout/app.blade.php
@@ -38,4 +38,14 @@
 </div>
 
 </body>
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140284113-1"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-140284113-1');
+</script>
+<!-- End Global site tag (gtag.js) - Google Analytics -->
 </html>
