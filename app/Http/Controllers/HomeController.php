2b5bd2bb508fdc0b37615943f1474c392a1555ed
Syed Ali <s.cuteali@gmail.com>
2019-05-16 03:33:00 +0500

    location

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,14 +7,20 @@ use App\Country;
 use Carbon\Carbon;
 use DateTimeZone;
 use Illuminate\Http\Request;
+use Stevebauman\Location\Facades\Location;
 
 class HomeController extends Controller
 {
     public function index()
     {
 //        dd(Carbon::now('PK'));
-        $ip = file_get_contents("http://ipecho.net/plain");
-        $url = 'http://ip-api.com/json/'.$ip;
+//        $ip = file_get_contents("http://ipecho.net/plain");
+        $ip_addres = \Request::ip();
+//        dd($ip_addres);
+//              $position = Location::get('217.69.3.154');
+        $position = Location::get($ip_addres);
+//        $country=Country::query()->where('country_code',$position->countryCode)->first();
+        $url = 'http://ip-api.com/json/'.$ip_addres;
         $userInfo = file_get_contents($url);
         $userInfo = json_decode($userInfo,true);
         $currentTime = Carbon::now($userInfo['timezone']);
